a9de45bf45f902d0b2ed24784b561672a16b09e8
SammyCode332 <samueloroja123@gmail.com>
2026-04-13 00:40:19 +0100

    updated auth yh

--- routers/authentication.py
+++ routers/authentication.py
@@ -8,6 +8,7 @@ router = APIRouter(
     tags = ['Authentication']
 )
 
+
 @router.post('/login')
 def login(request:OAuth2PasswordRequestForm=Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
